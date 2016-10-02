defe028ecf9fda9de6145e64453d76d27116f637
Gordondalos <gordondalos@gmail.com>
2016-10-02 20:20:14 +0600

    Реализовал роутинг

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent }    from './heroes.component';
 import { HomeComponent } from "./home.component";
 
-
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'heroes', component: HeroesComponent }
 ];
+
 export const appRoutingProviders: any[] = [
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
